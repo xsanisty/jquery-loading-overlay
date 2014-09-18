2f1d059b994635c5c359ee1a79043d248111a696
Ikhsan Agustian <ikhsan017@gmail.com>
2014-09-18 08:28:31 +0700

    initial jquery-loading-overlay

---
+++ jquery.loading-overlay.js
@@ -0,0 +1,59 @@
+(function($){
+    var defaultConfig = {
+        opacity     : 0.8,
+        image       : 'image/loader.gif',
+        zIndex      : 5000,
+        imageHeight : 32,
+        text        : ''
+    };
+
+    var loader = {
+        show : function(config){
+            var conf = $.extend({}, defaultConfig, config);
+            return this.each(function(){
+                var container = $(this);
+                var overlay   = $('<div>');
+                var image     = $('<img>');
+
+                container.css('position', 'relative');
+                overlay.css({
+                    position        :'absolute',
+                    top             : 0,
+                    left            : 0,
+                    height          : container.height(),
+                    width           : container.width(),
+                    opacity         : conf.opacity,
+                    zIndex          : conf.zIndex,
+                    backgroundColor : '#000000',
+                    textAlign       : 'center'
+                }).addClass('xsanisty-loading-overlay');
+
+                image.attr('src', conf.image).css({
+                    zIndex      : conf.zIndex+10,
+                    height      : conf.imageHeight,
+                    marginTop   : (container.height()/2) - (conf.imageHeight/2)
+                });
+
+                overlay.append(image);
+                container.append(overlay);
+
+            });
+        },
+        hide : function(){
+            return this.each(function(){
+                $(this).find('.xsanisty-loading-overlay').remove();
+            });
+        }
+    }
+
+    $.fn.loader = function($action){
+        if (loader[$action]) {
+            return loader[$action].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof($action) == 'object' || typeof($action) == 'undefined') {
+            var $loader = loader.show.apply(this, arguments);
+            return $loader;
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.loader');
+        }
+    }
+})(jQuery)
---
+++ readme.md
@@ -0,0 +1,66 @@
+###Overview
+
+jQuery loading overlay is a simple helper function wrapped as jQuery plugin for displaying loading
+indicator overlaid to the element where this plugin is initiated.
+
+code sample below will clearly dercribe it
+
+```javascript
+$('#somediv').loader();
+```
+
+this will create an overlaid element with loading image on the top of ```#somediv```,
+you can display it, configure it, and hide it
+
+###Method
+
+jQuery loading overlay has only two methods, ```show``` and ```hide``` which is responsible to display
+or hide the loading overlay. When no method is passed, it will call ```show``` method implicitly.
+
+####Show
+
+As the method name describe what it does, show method will show loading indicator overlaid to the selected element
+
+```javascript
+//this will diplay the loadng overlay on the top of #selector element
+$('#selector').loader('show');
+
+//this will have same effect on the code above
+$('#selector').loader();
+
+```
+
+####Hide
+
+Hide method is used to hide (actually remove it :D ) the loading overlay from the selected element.
+
+```javascript
+
+//this will hide the loading overlay from the selected element
+$('#selector').loader('hide');
+
+```
+
+###Configuration
+
+As mentioned above, you can configure how the loading overlay will be displayed. The default configuration
+is shown as below
+
+```javascript
+defaultConfig = {
+    //opacity level of the overlaid div
+    opacity     : 0.8,
+
+    //image used for the loading indicator
+    image       : 'image/loader.gif',
+
+    //the z-index of the overlay element
+    zIndex      : 5000,
+
+    //the loading indicator image height, to calculate the vertical center
+    imageHeight : 32,
+
+    //text to be displayed under the loading indicator image
+    text        : ''
+};
+```
